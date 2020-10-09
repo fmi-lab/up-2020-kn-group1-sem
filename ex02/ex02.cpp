80b566522e3db1984e44f3c73ab1c58af61f0261
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2020-10-09 13:43:17 +0300

    Ex 02: First program; cout/cin; Variables

---
+++ ex02/ex02.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int main() { // MAIN FUNCTION
+	cout << "Hello World" << endl;
+	cout << "Hello" << " " << "World" << endl;
+	cout << (1 * (2 + 3) * ((4 - 5) * 6) / 2) << endl; // constants
+	// variables (променливи)
+	
+	// ctrl + / (code blocks)
+	// ctrl + k + c (visual studio)
+	int a; // int == integer == само цели числа
+	cout << "Please enter value for a: ";
+	cin >> a;
+
+	int b;
+	cout << "Please enter value for b: ";
+	cin >> b;
+
+	cout << (a * a + b * b) << endl;
+
+	int c = 100000000000;
+	cout << "c: " << c << endl;
+
+	// float, double = 3.14
+	// char
+
+	return 0;
+}
+
+
+
+
